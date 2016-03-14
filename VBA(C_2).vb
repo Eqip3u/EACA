87801cb92771b49467e3e32b5dd767e064db3f5c
Roman Bushuev <Eqip3u.dem@yandex.ru>
2016-03-14 15:33:15 +0500

    VBA(C_2)

---
+++ VBA(C_2).vb
@@ -0,0 +1,15 @@
+﻿Module Module1
+    Sub Main()
+        Dim M As Integer, N As Integer
+        M = Val(InputBox("Введите первое число"))
+        N = Val(InputBox("Введите второе число"))
+        Do While M <> N
+            If M > N Then
+                M = M - N
+            Else
+                N = N - M
+            End If
+        Loop
+        MsgBox("НОД =" & M)
+    End Sub
+End Module
