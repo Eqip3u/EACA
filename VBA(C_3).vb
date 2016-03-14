711bc49ae7032437d133b3c1a1fab94b8d8268d0
Roman Bushuev <Eqip3u.dem@yandex.ru>
2016-03-14 17:16:53 +0500

    Added files via upload

---
+++ VBA(C_3).vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+    Sub Main()
+        Dim a, b, c, x1, x2, D
+        a = Val(InputBox("Введите коэффициет a"))
+        b = Val(InputBox("Введите средний коэффициент b"))
+        c = Val(InputBox("Введите свободный член с"))
+        D = b ^ 2 - 4 * a * c
+        If (D >= 0) Then
+            x1 = (-b + Sqr(D)) / (2 * a)
+            x2 = (-b - Sqr(D)) / (2 * a)
+            MsgBox("x1=" & x1)
+            MsgBox("x2=" & x2)
+        Else
+            MsgBox("Действительных корней не существует")
+        End If
+    End Sub
+End Module
